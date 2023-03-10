b78d22ee30652bfab907d894032d924ec0c8a6c7
notoiro <notoiro297@gmail.com>
2023-03-10 22:05:29 +0900

    temporary workaround to random voice stuck 3

--- index.js
+++ index.js
@@ -588,18 +588,16 @@ async function connect_vc(interaction){
   });
 
   // Temporary workaround to https://github.com/discordjs/discord.js/issues/9185
+  const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+    const newUdp = Reflect.get(newNetworkState, 'udp');
+    clearInterval(newUdp?.keepAliveInterval);
+  }
   connection.on('stateChange', (oldState, newState) => {
     const oldNetworking = Reflect.get(oldState, 'networking');
     const newNetworking = Reflect.get(newState, 'networking');
 
-    const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
-      const newUdp = Reflect.get(newNetworkState, 'udp');
-      clearInterval(newUdp?.keepAliveInterval);
-    }
-
     oldNetworking?.off('stateChange', networkStateChangeHandler);
     newNetworking?.on('stateChange', networkStateChangeHandler);
-
     logger.debug("Fixing...");
   });
 
