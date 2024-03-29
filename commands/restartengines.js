4bfbfef53c4be8db329c0f499da1611f7b56ab6b
notoiro297 <notoiro297@gmail.com>
2024-03-29 19:48:28 +0900

    1

---
+++ commands/restartengines.js
@@ -0,0 +1,23 @@
+const { execSync } = require('child_process');
+
+module.exports = {
+  data: {
+    name: "restartengines",
+    description: "[回避策]使用されているボイスエンジンを再起動します"
+  },
+  async execute(interaction) {
+    console.log(`restart exex: ${interaction.guild.id}`);
+
+    try{
+      const result_coeiroink = execSync('systemctl --user restart coeiroink.service');
+      const result_voicevox = execSync('systemctl --user restart vocievox.service');
+
+      console.log(result_coeiroink);
+      console.log(result_voicevox);
+    }catch(e){
+      console.log(e);
+    }
+
+    await interaction.reply('再起動を実行しました！');
+  },
+}
