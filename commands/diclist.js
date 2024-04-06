e5c8b87ec4055db6494f5dce735b88f4a43aed2a
notoiro297 <notoiro297@gmail.com>
2024-04-06 20:46:48 +0900

    8

--- commands/diclist.js
+++ commands/diclist.js
@@ -1,8 +1,50 @@
-const { ApplicationCommandOptionType } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+const app = require('../index.js');
 
 module.exports = {
   data: {
     name: "diclist",
     description: "辞書の単語一覧。",
   },
+
+  async execute(interaction){
+    const server_file = app.bot_utils.get_server_file(interaction.guild.id);
+    let dict = server_file.dict;
+
+    let list = "";
+    let is_limit = false;
+
+    for(let p = 0; p < 5; p++){
+      const tmp_dict = dict.filter(word => word[2] === p);
+
+      // limit of discord embed text length
+      if((list.length + `**${app.priority_list[p]}**\n`.length) > 1024){
+        is_limit = true;
+        break;
+      }else{
+        list += `**${app.priority_list[p]}**\n`;
+
+        for(let d of tmp_dict){
+          const s = `${d[0]} → ${d[1]}\n`;
+          if((s.length + list.length) > 1024){
+            is_limit = true;
+            break;
+          }else{
+            list += s;
+          }
+        }
+      }
+    }
+
+    const em = new EmbedBuilder()
+      .setTitle(`登録されている辞書の一覧です。`)
+      .addFields(
+        { name: "一覧", value: `${list}`},
+      );
+
+    if(is_limit) em.setDescription("表示上限を超えているため省略されています。");
+
+    await interaction.reply({ embeds: [em] });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -33,6 +33,12 @@ const {
 } = require('../config.json');
 
 module.exports = class App{
+  #priority_list = [ "最弱", "よわい", "普通", "つよい", "最強" ];
+
+  get priority_list(){
+    return Array.from(this.#priority_list);
+  }
+
   constructor(){
     this.remote_repalce = new RemoteReplace();
     this.logger = log4js.getLogger();
@@ -232,7 +238,6 @@ module.exports = class App{
         case "dicedit":
         case "dicdel":
         case "dicpriority":
-        case "diclist":
           if(command_name === "connect") command_name = "connect_vc";
           await this[command_name](interaction);
           break;
@@ -1046,42 +1051,5 @@ module.exports = class App{
     await interaction.reply({ embeds: [em] });
   }
 
-  async diclist(interaction){
-    const server_file = this.bot_utils.get_server_file(interaction.guild.id);
-    let dict = server_file.dict;
 
-    let list = "";
-    let is_limit = false;
-
-    for(let p = 0; p < 5; p++){
-      const tmp_dict = dict.filter(word => word[2] === p);
-
-      if((list.length + `**${priority_list[p]}**\n`.length) > 1024){
-        is_limit = true;
-        break;
-      }else{
-        list += `**${priority_list[p]}**\n`;
-
-        for(let d of tmp_dict){
-          const s = `${d[0]} → ${d[1]}\n`;
-          if((s.length + list.length) > 1024){
-            is_limit = true;
-            break;
-          }else{
-            list += s;
-          }
-        }
-      }
-    }
-
-    const em = new EmbedBuilder()
-      .setTitle(`登録されている辞書の一覧です。`)
-      .addFields(
-        { name: "一覧", value: `${list}`},
-      );
-
-    if(is_limit) em.setDescription("表示上限を超えているため省略されています。");
-
-    await interaction.reply({ embeds: [em] });
-  }
 }
