f685a702eda2458bca044a5f9e135530734bcfe4
notoiro297 <notoiro297@gmail.com>
2024-04-08 22:22:55 +0900

    14

--- src/index.js
+++ src/index.js
@@ -228,10 +228,8 @@ module.exports = class App{
       let command_name = interaction.commandName;
 
       switch(command_name){
-        case "connect":
         case "setvoiceall":
         case "currentvoice":
-          if(command_name === "connect") command_name = "connect_vc";
           await this[command_name](interaction);
           break;
         case "setspeed":
@@ -505,45 +503,6 @@ module.exports = class App{
     this.logger.debug(`remote replace: ${tmp_text}`);
 
     return tmp_text;
-  }
-
-  async connect_vc(interaction){
-    const guild = interaction.guild;
-    const member = await guild.members.fetch(interaction.member.id);
-    const member_vc = member.voice.channel;
-
-    if(!member_vc){
-      await interaction.reply({ content: "接続先のVCが見つかりません。" });
-      return;
-    }
-    if(!member_vc.joinable) {
-      await interaction.reply({ content: "VCに接続できません。" });
-      return;
-    }
-    if(!member_vc.speakable) {
-      await interaction.reply({ content: "VCで音声を再生する権限がありません。" });
-      return;
-    }
-
-    const guild_id = guild.id;
-
-    const current_connection = this.connections_map.get(guild_id);
-
-    if(current_connection){
-      await interaction.reply({ content: "接続済みです。" });
-      return;
-    }
-
-    const data = {
-      voice_id: member_vc.id,
-      text_id: interaction.channel.id,
-    }
-
-    await this._connect_vc(guild_id, data);
-
-    if(!this.status.debug){
-      await interaction.reply({ content: '接続しました。' });
-    }
   }
 
   async _connect_vc(guild_id, data){
