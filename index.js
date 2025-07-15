e4099b1578106e738135c7cda4b9e636adc9f798
notoiro297 <notoiro297@gmail.com>
2025-07-16 08:35:57 +0900

    feature: Node.jsのバージョンチェックを追加

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 "use strict";
 
+const { readFileSync } = require('fs');
+const path = require('path');
+const semver = require('semver');
+
+function assertNodeVersion() {
+  const pkgPath = path.resolve(process.cwd(), 'package.json');
+  const pkgJson = JSON.parse(readFileSync(pkgPath, 'utf-8'));
+
+  const requiredVersion = pkgJson.engines?.node;
+  if (!requiredVersion) return;
+
+  const currentVersion = process.version;
+  if (!semver.satisfies(semver.clean(currentVersion), requiredVersion)) {
+    console.error(
+      `🚫 Error: Node.js ${requiredVersion}以上が必要です。\n　 現在のバージョン: ${currentVersion}`
+    );
+    process.exit(1);
+  }
+}
+
+assertNodeVersion();
+
 const App = require('./src/index.js');
 
 function main(){
